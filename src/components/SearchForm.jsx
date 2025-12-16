ea1a8373046e4f8e669da096c6f05056ea64c9fc
zzxbcv <theoayman@gmail.com>
2025-12-16 17:39:35 +0000

    added all filters

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,27 +1,51 @@
 
 
-function SearchForm({ setFilters}) {
+function SearchForm() {
   return (
-    <form>
-      <label>
-        Property Type:
-        <select>
-          <option value="">Any</option>
-          <option value="House">House</option>
-          <option value="Flat">Flat</option>
-        </select>
-      </label>
+    <div>
+      <h1>Filters</h1>
+      <form>
+        <label>
+          Property Type:
+          <select>
+            <option value="">Any</option>
+            <option value="House">House</option>
+            <option value="Flat">Flat</option>
+          </select>
+        </label>
 
-      <label>
-        Min Price:
-        <input type="number" />
-      </label>
+        <label>
+          Minimum Price:
+          <input type="number" />
+        </label>
 
-      <label>
-        Max Price:
-        <input type="number" />
-      </label>
-    </form>
+        <label>
+          Maximum Price:
+          <input type="number" />
+        </label>
+
+        <label>
+          Number of Bedrooms:
+          <select>
+            <option value="">Any</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+            <option value="4">Four</option>
+          </select>
+        </label>
+
+        <label >
+          Date added:
+          <input type="date"></input>
+        </label>
+
+        <label>
+          Postcode
+          <input type="text"></input>
+        </label>
+      </form>
+    </div>
   );
 }
 
