b9677c324592f1c48863cfed3fef15d4c4cbee85
zzxbcv <theoayman@gmail.com>
2025-12-18 17:35:06 +0000

    specified bedroom amount

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -6,7 +6,7 @@ function PropertyCard({ property }) {
     <div className="propertycontainer">
       <div className="propertyCard">
         <img src={property.picture} alt={property.type} />
-        <h2>{property.type}</h2>
+        <h2>{property.bedrooms} bedroom {property.type}</h2>
         <p>{property.description}</p>
         <h3>{property.location}</h3>
         <h3>£{property.price.toLocaleString()}</h3>
