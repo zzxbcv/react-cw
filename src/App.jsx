b3fcb28743f2a2d2391f804f8ec063739a9a2771
zzxbcv <theoayman@gmail.com>
2026-01-02 16:03:07 +0000

    Prepared app for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import SearchPage from "./pages/SearchPage";
 import PropertyPage from "./pages/PropertyPage";
 import propertiesData from "./data/properties.json";
